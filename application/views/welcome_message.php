fbbd4177ef004187edbe2b5acb42f7f8a9f5a79a
sudipto <sudiptocsi@gmail.com>
2021-02-16 20:51:40 +0600

    updated

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -15,23 +15,71 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <body>
 
 	<div id="container">
-		<h1>Welcome to CodeIgniter!</h1>
-
-		<div id="body">
-			<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-			<p>If you would like to edit this page you'll find it located at:</p>
-			<code>application/views/welcome_message.php</code>
-
-			<p>The corresponding controller for this page is found at:</p>
-			<code>application/controllers/Welcome.php</code>
-
-			<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<h1>Task magnement list</h1>
+		<div class="row">
+			<div class="panel-wrapper">
+				<form>
+					<div class="form-field">
+						<div class="col-grid-1">
+							<label for="txtTaskName">Task Name : </label>
+						</div>
+						<div class="col-grid-3">
+							<input type="text" class="txtTaskName form-input" id="txtTaskName" placeholder="Task Name" />
+						</div>
+					</div>
+					<div class="form-field">
+						<div class="col-grid-1">
+							<label for="txtTaskDate">Task Date : </label>
+						</div>
+						<div class="col-grid-3">
+							<input type="text" class="txtTaskDate form-input" id="txtTaskDate" placeholder="Task Date" />
+						</div>
+					</div>
+					<div class="form-field">
+						<div class="col-grid-1">
+							<label for="txtTaskDescription">Task Description : </label>
+						</div>
+						<div class="col-grid-3">
+							<textarea class="txtTaskDescription form-input" id="txtTaskDescription" placeholder="Task Description">
+							</textarea>
+						</div>
+					</div>
+					<div class="form-field">
+						<button type="submit">Submit</button>
+					</div>
+				</form>
+			</div>
 		</div>
+		<div class="row">
+			<div class="column" style="background-color:#aaa;">
+				<h2>Column 1</h2>
+				<p>Some text..</p>
+			</div>
 
+			<div class="column" style="background-color:#bbb;">
+				<h2>Column 2</h2>
+				<p>Some text..</p>
+			</div>
+
+			<div class="column" style="background-color:#ccc;">
+				<h2>Column 3</h2>
+				<p>Some text..</p>
+			</div>
+
+			<div class="column" style="background-color:#ddd;">
+				<h2>Column 4</h2>
+				<p>Some text..</p>
+			</div>
+		</div>
 		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 	</div>
+	<script type="text/javascript" src="<?php echo site_url(); ?>js/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo site_url(); ?>js/jquery-ui.min.js"></script>
+	<script type="text/javascript">
+		$(function() {
 
+		});
+	</script>
 </body>
 
 </html>
